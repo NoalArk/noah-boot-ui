eab1e262442bc93d362e0ced049b2a07f1ef7792
noahark1996 <1284901081@qq.com>
2022-09-26 22:07:20 +0800

    == 首页配置

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,4 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-    <a-button type="primary">Primary Button</a-button>
-  </nav>
   <router-view/>
 </template>
 
@@ -16,16 +11,4 @@
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/views/index/AppIndex.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>15415</div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: "AppIndex",
+
+});
+</script>
+
+<style scoped>
+
+</style>
